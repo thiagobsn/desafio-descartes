bbe3e46a923c453d54a6e599f27f501987d91640
Thiago Bernardo <thiagobsn@gmail.com>
2023-06-01 00:29:20 -0300

    Ajuste

--- src/main/java/com/example/desafiodescartes/conf/ApplicationConfig.java
+++ src/main/java/com/example/desafiodescartes/conf/ApplicationConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class ApplicationConfig {
