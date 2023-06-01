46cf78f0bbaf616f5ba6989ba02f886f3c47f9ce
Thiago Bernardo <thiagobsn@gmail.com>
2023-06-01 00:31:46 -0300

    remoção de metodo não utlizados

--- src/main/java/com/example/desafiodescartes/domain/stopevent/repository/StopEventRepository.java
+++ src/main/java/com/example/desafiodescartes/domain/stopevent/repository/StopEventRepository.java
@@ -1,15 +1,11 @@
 package com.example.desafiodescartes.domain.stopevent.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.desafiodescartes.domain.stopevent.entity.StopEvent;
 
 public interface StopEventRepository extends JpaRepository<StopEvent, Long> {
 
-	public List<StopEvent> findAllByIdRouteOrderByIdDesc(Long idRoute);
-	
 	public StopEvent findTop1ByIdRouteOrderByInstantDesc(Long idRoute);
-
+	
 }
